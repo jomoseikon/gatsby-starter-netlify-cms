898e5278f300e9dc8e1ba7614e28c3cb754d867e
jomoseikon <seikon@jomoevent.com>
2022-01-07 15:00:20 +0900

    get back Features.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 const FeatureGrid = ({ gridItems }) => (
-  <div className="columns">
+  <div className="columns is-multiline">
     {gridItems.map((item) => (
-      <div key={item.text} className="column is-12-mobile is-6-tablet">
+      <div key={item.text} className="column is-6">
         <section className="section">
           <div className="has-text-centered">
             <div
